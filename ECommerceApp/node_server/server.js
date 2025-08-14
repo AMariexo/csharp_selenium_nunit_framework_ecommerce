372fe0b85908dc539f2f79cfe8aa52a9f11e8e61
aMXOFL <alyflmtbtrailapp@gmail.com>
2025-08-14 13:03:59 -0400

    moved node server out of test data folder
    moved node server out of test data folder and placed in a folder called node_server

---
+++ ECommerceApp/node_server/server.js
@@ -0,0 +1,15 @@
+//get packages
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.get("/login", (req, res) => {
+    const data = fs.readFileSync('./loginData.json');
+    res.setHeader('Content-Type', 'application/json');
+    res.json(JSON.parse(data));
+});
+
+app.listen(3000, () => {
+    console.log("API is running");
+        
+})
